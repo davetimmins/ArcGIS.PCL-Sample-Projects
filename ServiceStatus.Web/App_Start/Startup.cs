9b294341c812a3bdf1ec514cc95302f148cc05cd
Dave Timmins <dwtimmins@outlook.com>
2014-06-06 08:37:30 +1200

    Added service status example

---
+++ ServiceStatus.Web/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ServiceStatus.Web.App_Start.Startup))]
+namespace ServiceStatus.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            ArcGIS.ServiceModel.Serializers.JsonDotNetSerializer.Init();
+        }
+    }
+} 
---
+++ ServiceStatus.Web/ServiceHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using ArcGIS.ServiceModel;
+using System.Threading.Tasks;
+
+namespace ServiceStatus.Web
+{
+    public class ServiceHub : Hub
+    {
+        public async Task<String> checkStatuses(String serverUrl, String username, String password)
+        {
+            if (String.IsNullOrWhiteSpace(serverUrl) || String.IsNullOrWhiteSpace(username) || String.IsNullOrWhiteSpace(password)) return null;
+
+            var gateway = new SecureArcGISServerGateway(serverUrl, username, password);
+
+            var response = await gateway.DescribeSite();
+            
+            foreach (var resource in response.Services)
+            {
+                var status = await gateway.ServiceStatus(resource);
+                Clients.Caller.addNewServiceToPage(String.Format("{0} ({1})", resource.Name, resource.Type), status.Actual);
+            }
+
+            return String.Empty;
+        }
+    }
+}
